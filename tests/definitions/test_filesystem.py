a7c6076e68a9975ad8e48502edae6b90c587853e
Yannick Fricke <yannickfricke@googlemail.com>
2018-12-15 20:54:34 +0100

    test: 💍 Added test for the FileSystem class

---
+++ tests/definitions/test_filesystem.py
@@ -0,0 +1,10 @@
+from unittest import mock
+
+from definitions.FileSystem import FileSystem
+
+
+class TestFileSystem(object):
+    @mock.patch('os.listdir')
+    def test_get_directories(self, mocked_listdir):
+        FileSystem.get_directories('.')
+        mocked_listdir.assert_called_once_with('.')

aced62bee51ef66da7f3f5bb110033b0b27185d6
Yannick Fricke <yannickfricke@googlemail.com>
2018-12-15 20:54:17 +0100

    test: 💍 Added test for the PHP class

---
+++ tests/languages/test_php.py
@@ -0,0 +1,14 @@
+from languages.PHP import PHP
+
+php = PHP()
+
+
+class TestPHP(object):
+    def test_name(self):
+        assert php.name == 'php'
+
+    def test_pretty_name(self):
+        assert php.pretty_name == 'PHP'
+
+    def test_supportest_tools(self):
+        assert len(php.get_supported_tools()) == 0

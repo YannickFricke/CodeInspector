959ab48042627b77b8b6799c73ea02517d2dc212
Yannick Fricke <yannickfricke@googlemail.com>
2018-12-16 01:06:12 +0100

    test: 💍 Refactored the test for the CommandManager
    The patch function from unittest is now at the class level. Updated test
    for the execute command to test the call

--- tests/commands/test_commandmanager.py
+++ tests/commands/test_commandmanager.py
@@ -5,15 +5,14 @@ import pytest
 from pycin.definitions.CommandManager import CommandManager
 
 
+@mock.patch('pycin.commands.Command')
 class TestCommandManager:
-    @mock.patch('pycin.commands.Command.Command')
     def test_adding_a_command(self, mocked_command):
         command_manager = CommandManager()
         assert len(command_manager.get_commands()) == 0
         command_manager.add_command(mocked_command)
         assert len(command_manager.get_commands()) == 1
 
-    @mock.patch('pycin.commands.Command.Command')
     def test_removing_a_command(self, mocked_command):
         command_manager = CommandManager()
         command_manager.add_command(mocked_command)
@@ -21,24 +20,23 @@ class TestCommandManager:
         command_manager.remove_command(mocked_command)
         assert len(command_manager.get_commands()) == 0
 
-    @mock.patch('pycin.commands.Command.Command')
     def test_execute_a_command_with_the_given_name(self, mocked_command):
         mocked_command.configure_mock(name='Test')
         mocked_command.execute.return_value = True
 
         command_manager = CommandManager()
         command_manager.add_command(mocked_command)
+        command_manager.execute('Test', [])
 
-        assert command_manager.execute('Test', [])
+        mocked_command.execute.assert_called_once_with([])
 
-    @mock.patch('pycin.commands.Command.Command')
     def test_should_skip_not_matching_commands(self, mocked_command):
         with pytest.raises(Exception, message=r'.*Test.*'):
             command_manager = CommandManager()
             command_manager.add_command(mocked_command)
             command_manager.execute('test', [])
 
-    def test_throw_an_exception_when_no_command_was_found(self):
+    def test_throw_an_exception_when_no_command_was_found(self, mocked_command):
         with pytest.raises(Exception, message=r'.*Test.*'):
             command_manager = CommandManager()
             command_manager.execute('test', [])

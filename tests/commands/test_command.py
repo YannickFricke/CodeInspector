7c5685ae3eae43511f22180c2bcf8a51db77962b
Yannick Fricke <yannickfricke@googlemail.com>
2018-12-16 01:04:53 +0100

    test: 💍 Removed the test for Command#execute
    The function will be tested inside the CommandManager test

--- tests/commands/test_command.py
+++ tests/commands/test_command.py
@@ -6,17 +6,13 @@ class ExampleCommand(Command):
         super().__init__('Test')
 
     def execute(self, arguments):
-        return True
+        pass
 
 
 class TestCommand:
     def test_name(self):
         test_command = ExampleCommand()
         assert test_command.name == 'Test'
-
-    def test_execute(self):
-        test_command = ExampleCommand()
-        assert test_command.execute([])
 
     def test_str(self):
         command = ExampleCommand()
